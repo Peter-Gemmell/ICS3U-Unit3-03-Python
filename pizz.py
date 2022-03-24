620ff162881ee1606e3efc62ac436b7068a5489b
Peter Gemmell <74552992+Peter-Gemmell@users.noreply.github.com>
2022-03-24 09:33:30 -0400

    HawkMoon

--- pizz.py
+++ pizz.py
@@ -4,24 +4,27 @@
 # Created on March 2022
 # This program calculates the circumference of a circle using constants
 
-import constants
+import random
 
 
 def main():
-    # this function calculates the cost of pizza
+    # this program checks to see if the number guessed is the random number
 
     # input
-    diameter = int(input("Enter the diameter of the pizza you would like (inch): "))
-
-    # process
-    sub_total = constants.LABOR + constants.RENT + (diameter * constants.COST_PER_INCH)
-    total = sub_total + (sub_total * constants.HST)
-
-    # output
+    guess_Number = int(input("Enter a number(between 0-9): "))
     print("")
-    print("The cost of a {0} inch pizza is ${1:,.2f}".format(diameter, total))
-    print("\nDone.")
+    mystery_Number = random.randint(0, 9)  # a number between 0 and 9
+
+    # process & output
+    if guess_Number == mystery_Number:
+        print("You guessed correctly!")
+    else:
+        print("You guessed incorrectly. Try again")
+        print("")
+        print("The correct number was:{}".format(random_number))
+        print("\nDone.")
 
 
 if __name__ == "__main__":
     main()
+    
